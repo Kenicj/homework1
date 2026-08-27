16bfbf6998a4d7c7ed6dd845b882c6e7b5c6a030
учеба <makarenkovakatka@mail.ru>
2026-08-27 16:36:34 +0300

    Создала ветку

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        System.out.printf("Проверьте домашнюю работу!");
+        System.out.printf("Создала ветку!");
 
         }
     }
